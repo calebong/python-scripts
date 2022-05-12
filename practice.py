7396e50f5b54393322135f6d42685f9554333358
calebong <calebongjunyi@gmail.com>
2022-05-12 11:25:54 +0800

    practice

---
+++ practice.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import requests
+
+
+apiKey = "8cdcf870de9c79ffcb439e879b28a062f770eb58"
+
+
+
+headers = {'Content-Type': 'application/json'}
+
+
+vector = ["AAPL","TSLA","AXP","SHOP"]
+
+requestResponse = requests.get("https://api.tiingo.com/tiingo/daily/aapl/prices?startDate=2019-01-02&token=8cdcf870de9c79ffcb439e879b28a062f770eb58", headers=headers)
+
+print(requestResponse.json())
+
+out1 = requestResponse.json()
+
+
+outDf = pd.DataFrame(out1)
+
+outDf['date']
+
+outDf.describe()
+
+
+for p in range(2, 10):
+  print(p)
